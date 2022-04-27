00c2b1f73c5a2bf110ed619f9a10fe4626b593b5
Thomas Morrell <tmorrell@users.noreply.github.com>
2022-04-26 21:42:01 -0700

    Deps

--- create_big_record.py
+++ create_big_record.py
@@ -3,7 +3,7 @@ from caltechdata_api import caltechdata_edit
 from caltechdata_api import caltechdata_write
 from caltechdata_api import get_metadata
 from datacite import DataCiteRESTClient, schema43
-import datetime
+import datetime, requests
 
 url = "https://data.caltech.edu/records/"
 
